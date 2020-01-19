7d94bbef5b96901c546f849b0632e115340b2e52
kanzeparov <kanzeparov@yandex.ru>
2020-01-19 03:06:15 +0300

    cron install

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var config = require('./config.json');
 var TelegramBot = require('node-telegram-bot-api');
+var cron = require('node-cron');
 
 var token = config.token
 var bot = new TelegramBot(token, {polling: true});
 
 var notes = [];
+
+
 
 bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
     var userId = msg.from.id;
@@ -14,6 +17,33 @@ bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
     notes.push({ 'uid': userId, 'time': time, 'text': text });
 
     bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+
+    cron.schedule('*/1 * * * * *', () => {
+      bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+    });
+
+});
+
+bot.onText(/\/bookmark/, (msg, match) => {
+   const chatId = msg.chat.id;
+   const url = match.input.split(' ')[1];
+   // 'msg' is the received Message from Telegram
+   // 'match' is the result of executing the regexp above on the text content
+   // of the message
+
+   if (url === undefined) {
+       bot.sendMessage(
+           chatId,
+           'Please provide URL of article!',
+       );
+       return;
+   }
+
+   URLs.push(url);
+   bot.sendMessage(
+       chatId,
+       'URL has been successfully saved!',
+   );
 });
 
 setInterval(function(){

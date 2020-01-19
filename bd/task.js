b9c6d9f6d72be93fd765a8a45b77cca5f17c5dde
kanzeparov <kanzeparov@yandex.ru>
2020-01-19 06:59:05 +0300

    added

--- bd/task.js
+++ bd/task.js
@@ -3,7 +3,7 @@ const DATABASE_URL = 'postgres://postgres:postgres@localhost:5432/startblock';
 const database = new Sequelize(DATABASE_URL);
 
 const taskDB = database.define(
-  'task', {
+  'tasks', {
     nickname: {
       type: Sequelize.TEXT
     }
--- index.js
+++ index.js
@@ -3,70 +3,159 @@ var TelegramBot = require('node-telegram-bot-api');
 var cron = require('node-cron');
 const Sequelize = require('sequelize');
 const taskDB = require('./bd/task');
+var GoogleSpreadsheet = require('google-spreadsheet');
+var async = require('async');
+var config = require('./config.json');
+
+// spreadsheet key is the long id in the sheets URL
+var doc = new GoogleSpreadsheet(config.sheet_id);
+var sheet;
+var date = new Date();
 
 var token = config.token
 var bot = new TelegramBot(token, {polling: true});
+var name = "";
+var rStat = 0;
+var aStat = 0;
+var gStat = 0;
 
 
-bot.onText(/напомни/, function (msg, match) {
+bot.onText(/\/р (.+) ч (.+)/, function (msg, match) {
+    rStat = 1;
     var userId = msg.from.id;
-      bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)' + date);
-    cron.schedule('0 19 * * *', () => {
-      bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)' + date);
+    var task = match[1];
+    var time = match[2];
+    var today = new Date();
+    var date = today.getDate()+'.'+(today.getMonth()+1)+'.'+today.getFullYear();
+    async.series([
+      function setAuth(step) {
+        // see notes below for authentication instructions!
+        var creds = require('./google-generated-creds.json');
+        // OR, if you cannot save the file locally (like on heroku)
+        var creds_json = {
+          client_email: creds.client_email,
+          private_key: creds.private_key
+        }
+
+        doc.useServiceAccountAuth(creds, step);
+      },
+      function getInfoAndWorksheets(step) {
+        doc.getInfo(function(err, info) {
+          console.log('Loaded doc: '+info.title+' by '+info.author.email);
+          sheet = info.worksheets[0];
+          console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
+          step();
+        });
+      },
+      function addTime(step) {
+        doc.addRow(1, { date: date, task: task, time: time }, function(err) {
+      if(err) {
+        console.log(err);
+      }
+    });
+      }
+    ], function(err){
+        if( err ) {
+          console.log('Error: '+err);
+        }
     });
 });
 
-bot.onText(/\/start_test/, function (msg, match) {
-  var userId = msg.from.id;
-    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)' + date);
-  newQuestion(msg);
+bot.onText(/\/а (.+) ч (.+)/, function (msg, match) {
+    rStat = 1;
+    var userId = msg.from.id;
+    var task = match[1];
+    var time = match[2];
+    var today = new Date();
+    var date = today.getDate()+'.'+(today.getMonth()+1)+'.'+today.getFullYear();
+    async.series([
+      function setAuth(step) {
+        // see notes below for authentication instructions!
+        var creds = require('./google-generated-creds.json');
+        // OR, if you cannot save the file locally (like on heroku)
+        var creds_json = {
+          client_email: creds.client_email,
+          private_key: creds.private_key
+        }
+
+        doc.useServiceAccountAuth(creds, step);
+      },
+      function getInfoAndWorksheets(step) {
+        doc.getInfo(function(err, info) {
+          console.log('Loaded doc: '+info.title+' by '+info.author.email);
+          sheet = info.worksheets[1];
+          console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
+          step();
+        });
+      },
+      function addTime(step) {
+        doc.addRow(2, { date: date, task: task, time: time }, function(err) {
+      if(err) {
+        console.log(err);
+      }
+    });
+      }
+    ], function(err){
+        if( err ) {
+          console.log('Error: '+err);
+        }
+    });
 });
 
-bot.onText(/\/добавить_работу (.+)/, function (msg, match) {
-  var userId = msg.from.id;
-    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)' + date);
+bot.onText(/\/г (.+) ч (.+)/, function (msg, match) {
+    rStat = 1;
+    var userId = msg.from.id;
+    var task = match[1];
+    var time = match[2];
+    var today = new Date();
+    var date = today.getDate()+'.'+(today.getMonth()+1)+'.'+today.getFullYear();
+    async.series([
+      function setAuth(step) {
+        // see notes below for authentication instructions!
+        var creds = require('./google-generated-creds.json');
+        // OR, if you cannot save the file locally (like on heroku)
+        var creds_json = {
+          client_email: creds.client_email,
+          private_key: creds.private_key
+        }
 
-  //newQuestion(msg);
+        doc.useServiceAccountAuth(creds, step);
+      },
+      function getInfoAndWorksheets(step) {
+        doc.getInfo(function(err, info) {
+          console.log('Loaded doc: '+info.title+' by '+info.author.email);
+          sheet = info.worksheets[2];
+          console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
+          step();
+        });
+      },
+      function addTime(step) {
+        doc.addRow(3, { date: date, task: task, time: time }, function(err) {
+      if(err) {
+        console.log(err);
+      }
+    });
+      }
+    ], function(err){
+        if( err ) {
+          console.log('Error: '+err);
+        }
+    });
 });
 
-bot.onText(/\/показать_работы/, function (msg, match) {
-  var userId = msg.from.id;
-    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)' + date);
 
-  var info_job = taskDB.findAll({
-  attributes: ['name']
-});
-  bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)' + info_job);
-});
-
-bot.onText(/\/удалить_работу (.+)/, function (msg, match) {
-  var userId = msg.from.id;
-    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)' + date);
-
-  //ewQuestion(msg);
+bot.onText(/\/remind/, function (msg, match) {
+    var userId = msg.from.id;
+    bot.sendMessage(userId, 'Привет, в 22 00 не забудь про статус');
+    cron.schedule('0 19 * * *', () => {
+      bot.sendMessage(userId, 'статус 1 Что я делал сегодня? 2 Что планирую завтра? 3 Какие проблемы возникают?' + date);
+    });
 });
 
-
-bot.onText(/\/bookmark/, (msg, match) => {
-   const chatId = msg.chat.id;
-   const url = match.input.split(' ')[1];
-   // 'msg' is the received Message from Telegram
-   // 'match' is the result of executing the regexp above on the text content
-   // of the message
-
-   if (url === undefined) {
-       bot.sendMessage(
-           chatId,
-           'Please provide URL of article!',
-       );
-       return;
-   }
-
-   URLs.push(url);
-   bot.sendMessage(
-       chatId,
-       'URL has been successfully saved!',
-   );
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Сообщение получено, чтобы записать в эксель введи /(первая буква имени на русском) (Описание задачи с пробелами) ч (количество времени). Пример /р Работал над мониторингом и идеей ч 5');
 });
 
 bot.on("polling_error", (err) => console.log(err));
